30bf60dbf08b2dd2221e34e637a44364f9147559
kaique818 <kaiquematheus142005@gmail.com>
2024-11-14 20:19:23 -0300

    indo

--- app/src/main/java/br/com/etecia/estoquemobile/menu.java
+++ app/src/main/java/br/com/etecia/estoquemobile/menu.java
@@ -8,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textfield.TextInputEditText;
-
-public class MainActivity extends AppCompatActivity {
-    //declarar as variaveis
-    MaterialButton btnEntrar;
-    TextInputEditText txtSenha, txtEmail;
+public class menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login_main);
+        setContentView(R.layout.layout_menu);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        //apresentando as variaveis
-        btnEntrar = findViewById(R.id.btnEntrar);
-        txtSenha = findViewById(R.id.txtSenha);
-        txtEmail = findViewById(R.id.txtEmail);
-
-        //criando as açoes para o botao
-
     }
 }
