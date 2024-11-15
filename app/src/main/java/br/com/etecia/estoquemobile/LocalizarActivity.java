b64c00194cd54f963bb6569c269bc76d3e5f1320
kaique818 <kaiquematheus142005@gmail.com>
2024-11-14 21:50:27 -0300

    indo de novo 2

--- app/src/main/java/br/com/etecia/estoquemobile/LocalizarActivity.java
+++ app/src/main/java/br/com/etecia/estoquemobile/LocalizarActivity.java
@@ -1,8 +1,6 @@
 package br.com.etecia.estoquemobile;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,31 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.appbar.MaterialToolbar;
+public class LocalizarActivity extends AppCompatActivity {
 
-public class menu extends AppCompatActivity {
-
-    MaterialToolbar IdToolBarEstoque;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.layout_menu);
+        setContentView(R.layout.localizar_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        IdToolBarEstoque = findViewById(R.id.IdToolBarEstoque);
-
-        //clicar no botão
-
-        IdToolBarEstoque.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplication(),MainActivity.class));
-                finish();
-            }
         });
     }
 }
--- app/src/main/java/br/com/etecia/estoquemobile/MainActivity.java
+++ app/src/main/java/br/com/etecia/estoquemobile/MainActivity.java
@@ -1,6 +1,9 @@
 package br.com.etecia.estoquemobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,6 +35,22 @@ public class MainActivity extends AppCompatActivity {
         txtEmail = findViewById(R.id.txtEmail);
 
         //criando as açoes para o botao funcionar
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+                email = txtEmail.getText().toString().trim();
+                password = txtSenha.getText().toString().trim();
+
+                if (email.equals("etecia") && password.equals("etecia")) {
+                    startActivity(new Intent(getApplicationContext(), menu.class));
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Usuário ou senha inválidos", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
 
     }
 }
